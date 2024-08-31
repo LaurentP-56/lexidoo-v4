cd496c8d5d660dbdc5c177be9933154cdaa01ae9
LaurentP-56 <94957676+LaurentP-56@users.noreply.github.com>
2024-08-31 05:18:20 +0200

    Modification linked to app/livewire/MyGameComponent.php
    - Added a time countdown
    - Added end-of-game summary

--- resources/views/livewire/my-game-component.blade.php
+++ resources/views/livewire/my-game-component.blade.php
@@ -20,9 +20,8 @@
                             <div class="flex flex-wrap justify-center gap-4">
                                 @foreach ($levels as $level)
                                     <button type="button" wire:click="selectOption('level', {{ $level->id }})"
-                                        class="rounded bg-blue-500 active:bg-blue-600 hover:bg-blue-700 py-2 px-4 text-white focus:ring-2 focus:ring-offset-2 w-full  ">
+                                        class="rounded bg-blue-500 active:bg-blue-600 hover:bg-blue-700 py-2 px-4 text-white focus:ring-2 focus:ring-offset-2 w-full">
                                         <span>{{ $level->label }} ({{ $level->sub_label }})</span>
-
                                     </button>
                                 @endforeach
                             </div>
@@ -37,22 +36,19 @@
                         </div>
 
                         <div class="gap-4">
-                            <h3 class="text-center py-4 text-xl text-sky-900 font-semibold">Choisissez le temps
-                                d'apprentissage</h3>
+                            <h3 class="text-center py-4 text-xl text-sky-900 font-semibold">Choisissez le temps d'apprentissage</h3>
                             <div class="flex flex-wrap justify-center space-y-4">
                                 @foreach ($tempsOptions as $temps)
                                     <button type="button" wire:click="selectOption('temps', {{ $temps['id'] }})"
-                                        class="rounded bg-blue-500 active:bg-blue-600 hover:bg-blue-700 py-2 px-4 text-white focus:ring-2 focus:ring-offset-2 w-full  ">
-                                        <span>{{ $temps['duree'] }}</span>
-
+                                        class="rounded bg-blue-500 active:bg-blue-600 hover:bg-blue-700 py-2 px-4 text-white focus:ring-2 focus:ring-offset-2 w-full">
+                                        <span>{{ $temps['duree'] }} minutes</span>
                                     </button>
                                 @endforeach
                             </div>
                         </div>
                     @endif
 
-                    <!-- Thème -->
-                    @if ($step === 3)
+                    @if ($step === 3 && count($themes) > 0)
                         <div class="rightBtn">
                             <a href="javascript:void(0)" wire:click="backToStep(2)" class="btn btn-primary btn-lg">
                                 Retour
@@ -61,119 +57,144 @@
                         <div class="gap-4">
                             <h3 class="text-center py-4 text-xl text-sky-900 font-semibold">Choisissez un thème</h3>
                             <div class="flex flex-wrap justify-center space-y-4">
-                                @if (count($themes) > 0)
-                                    @foreach ($themes as $themeId => $theme)
-                                        <button type="button" wire:click="selectOption('theme', {{ $themeId }})"
-                                            class="rounded bg-blue-500 active:bg-blue-600 hover:bg-blue-700 py-2 px-4 text-white focus:ring-2 focus:ring-offset-2 w-full ">
-                                            <span>{{ $theme }}</span>
-
-                                        </button>
-                                    @endforeach
-                                @endif
+                                @foreach ($themes as $themeId => $theme)
+                                    <button type="button" wire:click="selectOption('theme', {{ $themeId }})"
+                                        class="rounded bg-blue-500 active:bg-blue-600 hover:bg-blue-700 py-2 px-4 text-white focus:ring-2 focus:ring-offset-2 w-full">
+                                        <span>{{ $theme }}</span>
+                                    </button>
+                                @endforeach
                             </div>
                         </div>
                     @endif
 
-                    <!-- Catégorie -->
-                    @if ($step === 4)
+                    @if ($step === 4 && $categories && $categories->isNotEmpty())
                         <div class="rightBtn">
                             <a href="javascript:void(0)" wire:click="backToStep(3)" class="btn btn-primary btn-lg">
                                 Retour
                             </a>
                         </div>
                         <div class="gap-4">
-                            <h3 class="text-center py-4 text-xl text-sky-900 font-semibold">
-                                Choisissez une catégorie
-                            </h3>
+                            <h3 class="text-center py-4 text-xl text-sky-900 font-semibold">Choisissez une catégorie</h3>
                             <div class="flex flex-wrap justify-center space-y-4">
-                                @if (count($categories) > 0)
-                                    @foreach ($categories as $categoryId => $category)
-                                        <button type="button"
-                                            wire:click="selectOption('category', {{ $categoryId }})"
-                                            class="rounded bg-blue-500 active:bg-blue-600 hover:bg-blue-700 py-2 px-4 text-white focus:ring-2 focus:ring-offset-2 w-full ">
-                                            <span>{{ $category }}</span>
-
-                                        </button>
-                                    @endforeach
-                                @endif
+                                @foreach ($categories as $categoryId => $category)
+                                    <button type="button" wire:click="selectOption('category', {{ $categoryId }})"
+                                        class="rounded bg-blue-500 active:bg-blue-600 hover:bg-blue-700 py-2 px-4 text-white focus:ring-2 focus:ring-offset-2 w-full">
+                                        <span>{{ $category }}</span>
+                                    </button>
+                                @endforeach
                             </div>
                         </div>
                     @endif
 
-                    <!-- Sous-Catégorie -->
-                    @if ($step === 5)
+                    @if ($step === 5 && $subCategories && $subCategories->isNotEmpty())
                         <div class="rightBtn">
                             <a href="javascript:void(0)" wire:click="backToStep(4)" class="btn btn-primary btn-lg">
                                 Retour
                             </a>
                         </div>
                         <div class="gap-4">
-                            <h3 class="text-center py-4 text-xl text-sky-900 font-semibold">
-                                Choisissez une sous-catégorie
-                            </h3>
+                            <h3 class="text-center py-4 text-xl text-sky-900 font-semibold">Choisissez une sous-catégorie</h3>
                             <div class="flex flex-wrap justify-center space-y-4">
-                                @if (count($subCategories) > 0)
-                                    @foreach ($subCategories as $subCategoryId => $subCategory)
-                                        <button type="button"
-                                            wire:click="selectOption('subCategory', {{ $subCategoryId }})"
-                                            class="rounded bg-blue-500 active:bg-blue-600 hover:bg-blue-700 py-2 px-4 text-white focus:ring-2 focus:ring-offset-2 w-full ">
-                                            <span>{{ $subCategory }}</span>
-                                        </button>
-                                    @endforeach
-                                @endif
+                                @foreach ($subCategories as $subCategoryId => $subCategory)
+                                    <button type="button" wire:click="selectOption('subCategory', {{ $subCategoryId }})"
+                                        class="rounded bg-blue-500 active:bg-blue-600 hover:bg-blue-700 py-2 px-4 text-white focus:ring-2 focus:ring-offset-2 w-full">
+                                        <span>{{ $subCategory }}</span>
+                                    </button>
+                                @endforeach
                             </div>
                         </div>
                     @endif
 
-                    <!-- Word -->
-                    @if ($step == 6 && $currentWord)
-                        <div class="gap-4">
-                            <h3 class="text-center py-4 text-xl text-sky-900 font-semibold">
-                                A vous de jouer
-                            </h3>
-
-                            <div class="row">
-                                <div class="col-md-12">
-                                    <div class="bg-body-tertiary p-5 rounded">
-                                        <p class="lead"> {{ $currentWord }}</p>
-                                        <small>
-                                            Réfléchissez quelques secondes Puis cliquez pour voir la réponse
-                                        </small>
-                                        <a class="btn btn-lg btn-primary" wire:click="showNewAnswer()" href="#"
-                                            role="button">
-                                            Réponse »
-                                        </a>
-                                    </div>
-                                </div>
+  <div>
+                        @if ($step == 6 && $currentWord)
+                            <div class="rightBtn">
+                                <a href="javascript:void(0)" wire:click="backToStep(3)" class="btn btn-primary btn-lg">
+                                    Retour
+                                </a>
                             </div>
 
-                            @if ($showAnswer == true)
-                                <div class="row">
-                                    <div class="col-md-12">
-                                        <div class="bg-body-tertiary p-5 rounded">
-                                            <p class="lead"> {{ $currentTranslation }}</p>
+                            <div class="gap-4">
+                                <h3 class="text-center py-4 text-xl text-sky-900 font-semibold">
+                                    A vous de jouer
+                                </h3>
+
+                                @if ($isGameRunning)
+                                    <div wire:poll.1000ms="tick">
+                                        <h2>Temps restant : <span>{{ $this->getFormattedTime() }}</span></h2>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-12">
+                                            <div class="bg-body-tertiary p-5 rounded">
+                                                <p class="lead"> {{ $currentWord }}</p>
+                                                <small>
+                                                    Réfléchissez quelques secondes puis cliquez pour voir la réponse
+                                                </small>
+                                                <a class="btn btn-lg btn-primary" wire:click="showNewAnswer()" href="#" role="button">
+                                                    Réponse »
+                                                </a>
+                                            </div>
                                         </div>
                                     </div>
-                                </div>
-                                <div class="d-flex gap-2 justify-content-center py-5">
-                                    <a class="bg-success text-white rounded-pill px-3"
-                                        wire:click="updateProbability('know')" href="JavaScript:void(0)">
-                                        Je le savais
-                                    </a>
-                                    <a class=" bg-danger text-white rounded-pill px-3"
-                                        wire:click="updateProbability('dont_know')" href="JavaScript:void(0)">
-                                        Je ne le savais pas
-                                    </a>
-                                    <a class="bg-info rounded-pill text-white px-3"
-                                        wire:click="updateProbability('dont_want_to_learn')" href="JavaScript:void(0)">
-                                        Je ne veux pas apprendre ce mot
-                                    </a>
-                                </div>
-                            @endif
-                        </div>
-                    @endif
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+
+                                    @if ($showAnswer)
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="bg-body-tertiary p-5 rounded">
+                                                    <p class="lead"> {{ $currentTranslation }}</p>
+                                                    @if ($currentAudio)
+                                                        <audio controls>
+                                                            <source src="data:audio/wav;base64,{{ base64_encode($currentAudio) }}" type="audio/wav">
+                                                            Votre navigateur ne supporte pas l'élément audio.
+                                                        </audio>
+                                                    @endif
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <div class="d-flex gap-2 justify-content-center py-5">
+                                            <a class="bg-success text-white rounded-pill px-3"
+                                                wire:click="updateProbability('know')" href="JavaScript:void(0)">
+                                                Je le savais
+                                            </a>
+                                            <a class=" bg-danger text-white rounded-pill px-3"
+                                                wire:click="updateProbability('dont_know')" href="JavaScript:void(0)">
+                                                Je ne le savais pas
+                                            </a>
+                                            <a class="bg-info rounded-pill text-white px-3"
+                                                wire:click="updateProbability('dont_want_to_learn')" href="JavaScript:void(0)">
+                                                Je ne veux pas apprendre ce mot
+                                            </a>
+                                        </div>
+                                    @endif
+                                @else
+                                    <div>
+                                        <h2>Fin du jeu</h2>
+                                        <p>Total de mots : {{ $totalWords }}</p>
+                                        <p>Je le savais : {{ $knewWordClicks }}</p>
+                                        <p>Je ne le savais pas : {{ $didntKnowWordClicks }}</p>
+                                        <p>Je ne veux pas apprendre ce mot : {{ $dontWantToLearnClicks }}</p>
+                                    </div>
+                                @endif
+                            </div>
+                        @endif
+                    </div>
+
+
+<script>
+    document.addEventListener('livewire:load', function () {
+        Livewire.on('start-countdown', event => {
+            let timeLeft = event.timeLeft;
+            let countdownElement = document.getElementById('countdown');
+
+            let countdownInterval = setInterval(() => {
+                timeLeft--;
+                countdownElement.textContent = timeLeft;
+
+                if (timeLeft <= 0) {
+                    clearInterval(countdownInterval);
+                    Livewire.emit('endGame');
+                }
+            }, 1000);
+        });
+    });
+</script>
